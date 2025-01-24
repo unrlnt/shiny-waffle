f66e0382d0fb13e34ffe74f858c271269f324743
Dennis Kind <dennis.kind@networkcircle.com>
2025-01-24 11:44:35 +0100

    added DB connection and .env

--- import_db.py
+++ import_db.py
@@ -1,13 +1,17 @@
 import mysql.connector
 from mysql.connector import Error
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def create_connection():
     try:
         connection = mysql.connector.connect(
-            host="your_host",
-            user="your_user",
-            password="your_password",
-            database="db_name"
+            host = os.getenv('DB_HOST'),
+            user = os.getenv('DB_USER'),
+            password = os.getenv('DB_PASSWORD'),
+            database = os.getenv('DB_NAME')
         )
         if connection.is_connected():
             print("Connected to the database")
